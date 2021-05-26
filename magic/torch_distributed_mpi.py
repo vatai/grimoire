1b3d50fe8ddd2f79a90397bd94cdce2ab5a471e3
Emil VATAI <emil.vatai@gmail.com>
2021-05-27 08:51:30 +0900

    Try without `init_process`

--- magic/torch_distributed_mpi.py
+++ magic/torch_distributed_mpi.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 """run.py:"""
-import os
+# import os
 import torch
 import torch.distributed as dist
-from torch.multiprocessing import Process
+
+# from torch.multiprocessing import Process
+
 
 def run(rank, size):
-    """ Distributed function to be implemented later. """
+    """Distributed function to be implemented later."""
     tensor = torch.zeros(1)
     if rank == 0:
         tensor += 1
@@ -16,20 +18,20 @@ def run(rank, size):
     else:
         # Receive tensor from process 0
         dist.recv(tensor=tensor, src=0)
-    print(f'Rank {rank} (size {size}) has data ', tensor[0])
+    print(f"Rank {rank} (size {size}) has data ", tensor[0])
 
 
-def init_process(rank, size, fn, backend='gloo'):
-    """ Initialize the distributed environment. """
-    # os.environ['MASTER_ADDR'] = '127.0.0.1'
-    # os.environ['MASTER_PORT'] = '29500'
-    dist.init_process_group(backend, rank=rank, world_size=size)
-    # dist.init_process_group(init_method="mpi://", backend=backend, rank=rank, world_size=size)
-    # fn(rank, size)
+# def init_process(rank, size, fn, backend="gloo"):
+#     """Initialize the distributed environment."""
+#     # os.environ['MASTER_ADDR'] = '127.0.0.1'
+#     # os.environ['MASTER_PORT'] = '29500'
+#     dist.init_process_group(backend, rank=rank, world_size=size)
+#     # dist.init_process_group(init_method="mpi://", backend=backend, rank=rank, world_size=size)
+#     # fn(rank, size)
 
 
 if __name__ == "__main__":
     available = dist.is_mpi_available()
     print(f"mpi available: {available}")
-    init_process(-1, -1, run, backend='mpi')
+    # init_process(-1, -1, run, backend="mpi")
     print(f"RANK: {dist.get_rank()}, SIZE: {dist.get_world_size()}")
