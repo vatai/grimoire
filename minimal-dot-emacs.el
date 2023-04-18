2fc1381be89c3bf9f6d5bf063d711b5b99847f8c
Emil VATAI <emil.vatai@gmail.com>
2023-04-18 09:29:29 +0900

    Update minimal-dot-emacs.el

--- minimal-dot-emacs.el
+++ minimal-dot-emacs.el
@@ -3,8 +3,25 @@
                          ("melpa" . "https://melpa.org/packages/"))
       package-enable-at-startup nil)
 
+(package-initialize)
 (let ((package 'use-package))
   (unless (package-installed-p package)
     (package-install package)))
 (eval-when-compile
   (require 'use-package))
+
+
+(use-package pdf-tools :ensure t
+  :custom
+  (pdf-annot-activate-created-annotations t)
+  :config
+  (pdf-loader-install)
+  ;; Disable swiper
+  (define-key pdf-view-mode-map (kbd "C-s") 'isearch-forward))
+(use-package org-pdftools)
+;; (use-package org-noter-pdftools)
+(use-package org-noter
+  :ensure t
+  :custom (org-noter-notes-search-path '("~/org/roam/references/"))
+  :config
+  (org-noter))
