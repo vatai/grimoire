688d1d231bf5019461aa82c5feb1423b531c8386
Emil VATAI <emil.vatai@gmail.com>
2023-04-18 09:10:48 +0900

    Add minimal-dot-emacs.el

---
+++ minimal-dot-emacs.el
@@ -0,0 +1,10 @@
+
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/"))
+      package-enable-at-startup nil)
+
+(let ((package 'use-package))
+  (unless (package-installed-p package)
+    (package-install package)))
+(eval-when-compile
+  (require 'use-package))
