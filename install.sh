b8fee9018eb0fdd8ee7b959394de08c29b6a9bca
Emil VATAI <emil.vatai@gmail.com>
2026-01-01 13:23:13 +0900

    [!] Update install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,28 @@
 #!/usr/bin/bash
 
-function do_dotfiles {
-    DOT_FILES=".bashrc .zshrc .emacs .ssh/config .mbsyncrc .latexmkrc .config/spotifyd .config/kitty .config/oama .gitconfig .screenrc"
+set -x
 
-    for FILE in ${DOT_FILES}; do
-        rm -rf "${HOME}/${FILE}"
+function do_dotfiles {
+    mapfile -t DOT_FILES < \
+            <(find -maxdepth 1 -type f -name '.*' ! -name '.git*' -or -name '.gitconfig'
+              find .config -mindepth 1 -maxdepth 1
+              echo .ssh/config)
+
+    for FILE in "${DOT_FILES[@]}"; do
+        rm -rf "${HOME:?}/${FILE}"
         ln -sf "$(pwd)/${FILE}" "${HOME}/${FILE}"
     done
 }
 
 function do_syncfiles {
-    SYNC_FILES="org .authinfo.gpg .password-store"
+    SYNC_FILES=(
+        org
+        .authinfo.gpg
+        .password-store
+        )
 
-    for FILE in ${SYNC_FILES}; do
-        [ -L ${HOME}/${FILE} ] || ln -sf "${HOME}/Sync/${FILE}" "${HOME}/${FILE}"
+    for FILE in "${SYNC_FILES[@]}"; do
+        [ -L "${HOME}/${FILE}" ] || ln -sf "${HOME}/Sync/${FILE}" "${HOME}/${FILE}"
     done
 }
 
@@ -33,4 +42,4 @@ do_dotfiles
 do_syncfiles
 
 do_files "ln -sf" system/home ${HOME}
-do_files "sudo ln -sf" system/etc /etc
+# do_files "sudo ln -sf" system/etc /etc
